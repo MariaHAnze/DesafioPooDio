eed58daa305622d4dd1b9d6ca1f9d5b65ef91de8
MariaHAnze <mariah.93@gmail.com>
2022-08-03 16:44:27 -0300

    first commit

---
+++ src/projetoDioPoo/Main.java
@@ -0,0 +1,38 @@
+package projetoDioPoo;
+
+import projetoDioPoo.dominio.Curso;
+import projetoDioPoo.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java");
+        curso1.setDescricao("Curso de Java");
+        curso1.setCargaHoraria(40);
+        System.out.println(curso1);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Python");
+        curso2.setDescricao("Curso de Python");
+        curso2.setCargaHoraria(10);
+        System.out.println(curso2);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Java");
+        mentoria1.setDescricao("Mentoria de Java");
+        mentoria1.setData(LocalDate.now());
+        System.out.println(mentoria1);
+
+        Mentoria mentoria2 = new Mentoria();
+        mentoria2.setTitulo("Python");
+        mentoria2.setDescricao("Metoria de Python");
+        mentoria2.setData(LocalDate.now());
+
+
+
+
+    }
+}
---
+++ src/projetoDioPoo/dominio/Mentoria.java
@@ -0,0 +1,32 @@
+package projetoDioPoo.dominio;
+import java.time.LocalDate;
+
+
+public class Mentoria extends Conteudo  {
+    private String titulo;
+    private String descricao;
+    private LocalDate data;
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO + 20;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria :" +
+                getTitulo() + ", " +
+                getDescricao() +
+                ", será no dia " + getData() + '\n' +
+                "*************************************";
+    }
+
+}
